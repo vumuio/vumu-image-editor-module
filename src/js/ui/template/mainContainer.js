d834d1eec6905ba4416d6f6e02430177ace1a84b
ameerhamzaexponento <97496586+ameerhamzaexponento@users.noreply.github.com>
2022-02-02 10:06:48 +0500

    UI Modifications in editor is done. (#1)
    * UI Modifications in editor is done.
    
    * editor container width is decreased.

--- src/js/ui/template/mainContainer.js
+++ src/js/ui/template/mainContainer.js
@@ -5,10 +5,9 @@ export default ({
   loadButtonStyle,
   downloadButtonStyle,
   submenuStyle,
-}) => `
+}) => ` 
     <div class="tui-image-editor-main-container" style="${commonStyle}">
       <div class="page-container">
-        <div class="left-panel"></div>
         <div id="content">
           <div class="main-container">
               <div class="tui-image-editor-header" style="${headerStyle}">
@@ -27,19 +26,16 @@ export default ({
                       <div class="tui-image-editor-submenu-style" style="${submenuStyle}"></div>
                   </div>
                   <div class="tui-image-editor-wrap">
-                      <ul class="breadcrumbs list-none flex items-center">
-                        <li class="active"><a href="#">Personalised Dashboard</a></li>
-                        <li>Canvas</li>
-                      </ul>
                       <div class="tui-image-editor-size-wrap">
                       <div class="tui-image-editor-align-wrap">
                           <div class="tui-image-editor">
                             <input type="text" placeholder="Enter project title here..." class="project-title" />
                           </div>
-                          <div class="save-button">
-                            <button class="btn-primary">Save & Next</button>
-                          </div>
                       </div>
+
+                    </div>
+                    <div class="save-button">
+                        <button class="btn-primary">Save & Next</button>
                     </div>
                   </div>
               </div>
